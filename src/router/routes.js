346f8c296907a4dc802b093e0d368bc12c6a9849
garritfra <garritfranke@gmail.com>
2020-04-24 21:15:09 +0200

    Add helloworld route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,9 @@
 import App from "../App.vue";
+import HelloWorld from "../components/HelloWorld.vue";
 
-const routes = [{ path: "/", component: App }];
+const routes = [
+  { path: "/", component: App },
+  { path: "/helloworld", component: HelloWorld }
+];
 
 export default routes;
