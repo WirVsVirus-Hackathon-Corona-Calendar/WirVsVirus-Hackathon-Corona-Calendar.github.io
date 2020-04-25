4e6b7ba4727fcab5deff9aa63cce93443898c6b8
garritfra <garritfranke@gmail.com>
2020-04-25 14:15:40 +0200

    Add challengeView

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,13 @@
 import TodaysChallengeView from "../views/TodaysChallengeView.vue";
+import ChallengeView from "../views/ChallengeView.vue";
 
-const routes = [{ path: "/today", component: TodaysChallengeView }];
+const routes = [
+  {
+    path: "/challenges/:challengeId",
+    component: ChallengeView,
+    props: true
+  },
+  { path: "/today", component: TodaysChallengeView }
+];
 
 export default routes;
---
+++ src/views/ChallengeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Deine Challenge!</h1>
+    <p>{{challengeId}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChallengeView",
+  props: ["challengeId"]
+};
+</script>
+
+<style>
+</style>
