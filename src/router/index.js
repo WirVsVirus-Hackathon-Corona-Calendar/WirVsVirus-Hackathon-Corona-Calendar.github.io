96780e60a3ad1ad821bc745ceaf985c656282a6f
garritfra <garritfranke@gmail.com>
2020-04-25 12:25:58 +0200

    Set node to production mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import VueRouter from "vue-router";
 import routes from "./routes";
 
+console.log(process.env.NODE_ENV);
+
 const router = new VueRouter({
   mode: "history",
   base: process.env.NODE_ENV === "production" ? "/imfundfirus-frontend" : "/",
