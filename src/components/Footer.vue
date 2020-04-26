dd0b9ec9b2d1d9ea13078eb86414d97202c3bd78
Fabian <mail@fabian-duerkop.de>
2020-04-26 12:58:03 +0200

    Add Paypal link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -19,6 +19,13 @@
         href="https://github.com/WirVsVirus-Hackathon-Corona-Calendar"
       >
       </a>
+      <a
+        class="footer_social footer_paypal"
+        target="_blank"
+        href="https://paypal.me/pools/c/8nPKKPLDhb"
+      >
+        <span class="tooltiptext">Mit Paypal spenden!</span>
+      </a>
     </div>
     <a class="footer_link" href="/impressum">
       Impressum
@@ -102,6 +109,13 @@ export default {
   background-position: center;
 }
 
+.footer_paypal {
+  background-image: url("../assets/paypal_logo_black.png");
+  background-size: 29px 29px;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
 .footer_instagram:hover{
   background-image: url("../assets/instagram_logo_white.png");
   background-color: #F00075;
@@ -113,6 +127,41 @@ export default {
 .footer_github:hover{
   background-image: url("../assets/github_logo_white.png");
   background-color: #24292e;
+}
+.footer_paypal:hover{
+  background-image: url("../assets/paypal_logo_white.png");
+  background-color: #013089;
+}
+
+.footer_paypal .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  margin-top:-60px;
+  margin-left:-60px;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+}
+
+.footer_paypal .tooltiptext::after {
+  content: " ";
+  position: absolute;
+  top: 100%; /* At the bottom of the tooltip */
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: black transparent transparent transparent;
+}
+
+.footer_paypal:hover .tooltiptext {
+  visibility: visible;
 }
 
 @media only screen and (max-width: 500px){
